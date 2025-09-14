70c2363d43736e0150dcbd96f2998f3a5c0634f4
Sarada Ojha <saradaojha@live.com>
2025-09-14 21:29:14 +0530

    dropdwon added for country state and city and ui changes

--- Tourist.UI/src/app/auth/register/register.component.ts
+++ Tourist.UI/src/app/auth/register/register.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+
+
+  import { Component } from '@angular/core';
 
 import { RegisterService } from './services/register.service';
 import { RegisterModel } from './Models/register.model';
+import { Country, State, City } from 'country-state-city';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +13,9 @@ import { RegisterModel } from './Models/register.model';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  getSelectValue(event: Event): string {
+    return (event.target && (event.target as HTMLSelectElement).value) || '';
+  }
   model: RegisterModel = {
     firstName: '',
     middleName: '',
@@ -28,15 +34,48 @@ export class RegisterComponent {
 
   constructor(private registerService: RegisterService) {}
 
+  countries = Country.getAllCountries();
+  states: any[] = [];
+  cities: any[] = [];
+  onCountryChange(countryCode: string) {
+    this.model.country = countryCode;
+    this.states = State.getStatesOfCountry(countryCode);
+    this.model.state = '';
+    this.cities = [];
+    this.model.city = '';
+  }
+
+  onStateChange(stateCode: string) {
+    this.model.state = stateCode;
+    this.cities = City.getCitiesOfState(this.model.country, stateCode);
+    this.model.city = '';
+  }
+
   onSubmit(form: any) {
     this.submitted = true;
     this.successMessage = '';
     this.errorMessage = '';
     if (form.valid) {
-      this.registerService.register(this.model).subscribe({
+      // Prepare a copy of the model with names instead of codes
+      const countryObj = this.countries.find(c => c.isoCode === this.model.country);
+      const stateObj = this.states.find(s => s.isoCode === this.model.state);
+      const cityObj = this.cities.find(c => c.name === this.model.city);
+      const payload = {
+        ...this.model,
+        country: countryObj ? countryObj.name : '',
+        state: stateObj ? stateObj.name : '',
+        city: cityObj ? cityObj.name : ''
+      };
+      this.registerService.register(payload).subscribe({
         next: (res) => {
           this.successMessage = typeof res === 'string' ? res : (res && res.message ? res.message : 'Registration successful!');
           form.resetForm();
+          // Reset dropdowns to default
+          this.model.country = '';
+          this.model.state = '';
+          this.model.city = '';
+          this.states = [];
+          this.cities = [];
           this.submitted = false;
         },
         error: (err) => {
